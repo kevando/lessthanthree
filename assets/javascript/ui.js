094b7d12dbda8662c64c7a9c353ef416b27ee7a1
Kevin Habich <k3@Kevins-MacBook-Pro.local>
2020-08-25 23:11:37 -0700

    wip release

--- assets/javascript/ui.js
+++
@@ -1,9 +0,0 @@
-
-var $blue = "#4361EE";
-var $pink = "#F72585";
-var $purple = "#7209B7";
-var $orange = "#F86624";
-var $yellow = "#FFCA3A";
-var $red = "#EA3546";
-
-
---
+++ assets/js/form.js
@@ -0,0 +1,42 @@
+var db = firebase.firestore();
+var mediaType = window.location.pathname.split('/').pop()
+
+if(mediaType === "movie") mediaType = "🎬"
+if(mediaType === "book") mediaType = "📖"
+if(mediaType === "music") mediaType = "📻"
+
+$("#MediaType").text(mediaType);
+
+$("#MyForm").on("submit", function (event) {
+
+  console.log("form submitted");
+
+  event.preventDefault();
+
+  var data = {
+    firstPlace: $("input[name=firstPlace]").val() || '',
+    secondPlace: $("input[name=secondPlace]").val() || '',
+    mediaType: mediaType,
+    createdAt: new Date(),
+  }
+
+  console.log(data);
+
+  db
+    .collection("favorites-v1")
+    .add(data)
+    .then(function (docRef) {
+      $("#SubmitFormBtn").fadeOut()
+      $("input").fadeOut()
+      var url = "/fixer/" + docRef.id;
+      var displayUrl = "lessthanthreee.com/fixer/" + docRef.id;
+      $("#YourUrl").text(displayUrl)
+      $("#YourUrl").attr("href",url);
+    })
+    .catch(function (error) {
+      console.error("Error adding document: ", error);
+      $("#StatusText").text("There was an error: " + error);
+    });
+
+});
+
